0fd7f53a80a4b62eb846902037274752deed8ba2
jorabekbahronov <jorabekbahronov3@gmail.com>
2025-08-07 17:09:40 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,85 @@
+// exam misolari
+
+
+// 1masala
+// function makeNegative(num) {
+//   let a = num + 1
+//     let b = num - 1
+//       let t = 0
+
+//   if (a >= b) {
+//     console.log(b)
+//   } else if (t) {
+//     console.log(t)
+//   }   else (b) {
+//    console.log(b)
+//   }
+   
+
+//   }
+
+
+
+
+
+ 
+
+
+// makeNegative(1) // -1
+// makeNegative(7) // -7
+// makeNegative(-5) // -5
+// makeNegative(-6) // -6
+// makeNegative(0) // 0
+
+
+
+
+
+// // 2 masala
+
+//   function evenorOdd(number) {
+//     let even = number+2
+//     let odd = (number % 0) >= 5
+//     console.log(odd)
+//     console.log(even)
+//   }
+
+ 
+
+
+
+//  evenorOdd(2) // even
+//  evenorOdd(7) // odd
+
+
+
+
+  
+
+
+
+// 4 masala
+
+//   function solution (str) {
+   
+//   let arr = ["apple", "bannana", " dragonfruit", "grusha"].reverse()
+   
+//   console.log(arr)
+
+
+//   }
+
+//  solution ("world") // dlrow
+
+
+//  // 5 misol
+
+  // function positiveSum(arr) {
+    
+  //    let arr = [-1, -3, -2, -2 ].reduce((acc, num) => acc + num)
+
+  //  console.log(arr)
+
+  // }
+
+  // positiveSum ([-1, -3, -2, -2])
